47ef1e1a4309c686fcba57528222888e22a4ec93
anubhutibth <21f2000399@student.onlinedegree.iitm.ac.in>
2022-08-19 08:01:20 +0530

    Getting rough idea of api

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Student struct {
+	StudentId string `json:"studentId"`
+	Name      string `json:"Name"`
+	Age       int    `json:"Age"`
+	Class     string `json:"Class"`
+	Subject   string `json:"Subject"`
+}
+
+var Students []Student
 
 func main() {
-	fmt.Println("All good!")
+	Students = []Student{
+		{StudentId: "1", Name: "idk", Age: 6, Class: "nursery", Subject: "Poem"},
+	}
+	handleRequests()
+}
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "homepage!")
+	fmt.Println("Endpoint hit1")
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homepage)
+	myRouter.HandleFunc("/all", returnAllStudents)
+	myRouter.HandleFunc("/student", addNewStudent).Methods("POST")
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func addNewStudent(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := ioutil.ReadAll(r.Body)
+	//fmt.Fprintf(w, "%+v", string(reqBody))
+	var student Student
+	json.Unmarshal(reqBody, &student)
+	Students = append(Students, student)
+	json.NewEncoder(w).Encode(student)
+	fmt.Println("Endpoint hit2")
+	if err != nil {
+		fmt.Println("Could not add student")
+	}
+}
+
+func returnAllStudents(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint hit3")
+	json.NewEncoder(w).Encode(Students)
 }
